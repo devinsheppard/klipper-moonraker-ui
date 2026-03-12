b79c54f0832f109f5384e412b780c9dd392b7b4a
Devin Sheppard <dksheppard77@gmail.com>
2026-03-11 21:52:19 -0500

    GCode Files button fixes and add folders

--- src/moonraker.js
+++ src/moonraker.js
@@ -94,7 +94,7 @@ export class MoonrakerClient {
 
     this.ws.addEventListener("open", () => {
       this.setConnectionState("connected");
-      this.send({ method: "printer.objects.subscribe", params: { objects: { print_stats: null, virtual_sdcard: null, gcode_move: null, motion_report: null, extruder: null, heater_bed: null, toolhead: null, manual_probe: null } } });
+      this.send({ method: "printer.objects.subscribe", params: { objects: { print_stats: null, virtual_sdcard: null, gcode_move: null, motion_report: null, extruder: null, heater_bed: null, toolhead: null, manual_probe: null, stepper_enable: null } } });
     });
 
     this.ws.addEventListener("close", () => this.setConnectionState("disconnected"));
@@ -771,6 +771,7 @@ export class MoonrakerClient {
     return this.deleteFile("logs", path);
   }
 }
+
 
 
 
